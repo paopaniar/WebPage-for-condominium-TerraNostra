b46bdc6ca37d0ac79640de3266708aa9c14cf962
paopaniar <panipao0@gmail.com>
2023-02-11 16:41:06 -0600

    error en el ViewBag please review

--- TerraNostra/App_Start/WebApiConfig.cs
+++ TerraNostra/App_Start/WebApiConfig.cs
@@ -1,33 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http.Formatting;
-using System.Net.Http.Headers;
 using System.Web.Http;
 
-namespace TerraNostra
+namespace Web
 {
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-            config.MapHttpAttributeRoutes();
-            config.Routes.MapHttpRoute(
-            name: "DefaultApi",
-            routeTemplate: "api/{controller}/{action}/{id}",
-            defaults: new { id = RouteParameter.Optional }
-            );
-            GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
-            // Adding formatter for Json
-            // /api/Controller/method/parameter/type=json
-            config.Formatters.JsonFormatter.MediaTypeMappings.Add(new QueryStringMapping("type", "json",
-            new MediaTypeHeaderValue("application/json")));
-            // Adding formatter for XML
-            // /api/Controller/method/parameter/type=xml
-            config.Formatters.XmlFormatter.MediaTypeMappings.Add(new QueryStringMapping("type", "xml",
-            new MediaTypeHeaderValue("application/xml")));
-        }
 
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
     }
 }
