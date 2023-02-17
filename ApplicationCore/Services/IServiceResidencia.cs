15e34844d0d695693f2ffb2429f3bd3c21c7ba87
MARIANELA ROMERO MENDEZ <mromerom@est.utn.ac.cr>
2023-02-17 17:57:21 -0600

    residencia

--- ApplicationCore/Services/IServiceResidencia.cs
+++ ApplicationCore/Services/IServiceResidencia.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ApplicationCore.Services
 {
-	class IServiceResidencia
+	public interface IServiceResidencia
 	{
 
 	}
--- ApplicationCore/Services/ServiceResidencia.cs
+++ ApplicationCore/Services/ServiceResidencia.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Infraestructure.Models;
 
 namespace ApplicationCore.Services
 {
-    class ServiceResidencia
+   public class ServiceResidencia : IServiceResidencia
     {
-
+        public IEnumerable<residencia> GetResidencia()
+        {
+            IRepositoryResidencia repository = new RepositoryResidencia();
+            return repository.GetResidencia();
+        }
     }
 }
