237ec91ee51001ad922d72881eab6e21760eac40
paopaniar <panipao0@gmail.com>
2023-02-17 18:11:09 -0600

    residencia repositorio

---
+++ Infraestructure/Repository/RepositoryResidencia.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infraestructure.Models;
+
+namespace Infraestructure.Repository
+{
+    public class RepositoryResidencia : IRepositoryResidencia
+    {
+        public void DeleteResidencia(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<residencia> GetResidencia()
+        {
+            throw new NotImplementedException();
+        }
+
+        public residencia GetResidenciaByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<residencia> GetResidencias()
+        {
+            return null;
+        }
+    }
+    }
