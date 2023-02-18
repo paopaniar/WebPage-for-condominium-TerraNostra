9f0451679739bdcb278323d1cea7df6ea0207575
paopaniar <panipao0@gmail.com>
2023-02-18 16:39:50 -0600

    push repo plancobro

---
+++ Infraestructure/Repository/RepositoryPlanCobro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infraestructure.Models;
+using Infraestructure.Utils;
+
+namespace Infraestructure.Repository
+{
+    class RepositoryPlanCobro
+    {
+    }
+}
