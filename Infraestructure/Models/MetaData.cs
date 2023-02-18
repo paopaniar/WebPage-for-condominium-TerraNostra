5aff9146001b8c45b410dcd037409b14d77655bf
paopaniar <panipao0@gmail.com>
2023-02-18 15:06:16 -0600

    metadata updated

--- Infraestructure/Models/MetaData.cs
+++ Infraestructure/Models/MetaData.cs
@@ -9,7 +9,25 @@ namespace Infraestructure.Models
 {
 	internal partial class ResidenciaMetaData 
 	{
-		
-	}
+        public int id { get; set; }
+        public int usuario { get; set; }
+        public int individualsNumber { get; set; }
+        public string annio { get; set; }
+        public int estado { get; set; }
+        public string otherInfoDetails { get; set; }
+        public Nullable<int> numeroCasa { get; set; }
+    }
+
+    internal partial class UsuarioMetaData
+    {
+        public int rolId { get; set; }
+        public int identificacion { get; set; }
+        public string telefono { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string apellido2 { get; set; }
+        public Nullable<int> estado { get; set; }
+
+    }
 
 }
