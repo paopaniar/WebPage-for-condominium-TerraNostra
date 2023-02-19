16323db9002d6aa3f954503dabc3c1aad05ee562
MARIANELA ROMERO MENDEZ <mromerom@est.utn.ac.cr>
2023-02-18 22:08:43 -0600

    plan residencia

---
+++ Infraestructure/Repository/IRepositoryPlanResidencia.cs
@@ -0,0 +1,17 @@
+﻿using Infraestructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Repository
+{
+	public interface IRepositoryPlanResidencia
+	{
+		plan_residencia GetPlanResidenciaByID(int id);
+		IEnumerable<plan_residencia> GetPlanResidencia();
+
+
+	}
+}
---
+++ Infraestructure/Repository/RepositoryPlanResidencia.cs
@@ -0,0 +1,22 @@
+﻿using Infraestructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Repository
+{
+	public class RepositoryPlanResidencia : IRepositoryPlanResidencia
+	{
+		public IEnumerable<plan_residencia> GetPlanResidencia()
+		{
+			throw new NotImplementedException();
+		}
+
+		public plan_residencia GetPlanResidenciaByID(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
