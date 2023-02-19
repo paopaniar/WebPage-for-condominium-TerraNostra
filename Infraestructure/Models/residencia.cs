30723decdeb0cd0915e0ae5189c0e9271731dbe3
MARIANELA ROMERO MENDEZ <mromerom@est.utn.ac.cr>
2023-02-19 16:08:20 -0600

    meta

--- Infraestructure/Models/residencia.cs
+++ Infraestructure/Models/residencia.cs
@@ -11,7 +11,9 @@ namespace Infraestructure.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
+    [MetadataType(typeof(ResidenciaMetaData))]
     public partial class residencia
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
