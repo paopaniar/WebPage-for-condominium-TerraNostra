c5427363d533475dc2edb829180476f063b1f791
MARIANELA ROMERO MENDEZ <mromerom@est.utn.ac.cr>
2023-02-18 21:58:26 -0600

    cambios irrelevantes

---
+++ TerraNostra/Controllers/EstadosCuentaController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TerraNostra.Controllers
+{
+    public class EstadosCuentaController : Controller
+    {
+        // GET: EstadosCuenta
+        public ActionResult Index()
+        {
+            IEnumerable<residencia> lista = null;
+            try
+            {
+                IServiceResidencia _ServiceResidencia = new ServiceResidencia();
+                lista = _ServiceResidencia.GetResidencia();
+                ViewBag.title = "Residencias";
+                return View(lista);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, MethodBase.GetCurrentMethod());
+                TempData["Message"] = "Error al procesar los datos! " + ex.Message;
+
+                // Redireccion a la captura del Error
+                return RedirectToAction("Default", "Error");
+            }
+        }
+    }
+}
