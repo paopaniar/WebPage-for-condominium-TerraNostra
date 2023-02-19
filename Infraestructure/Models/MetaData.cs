587820ea1738052fd6c9e6afa84138b76ce54262
paopaniar <panipao0@gmail.com>
2023-02-18 20:23:35 -0600

    metaData changes

--- Infraestructure/Models/MetaData.cs
+++ Infraestructure/Models/MetaData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +11,31 @@ namespace Infraestructure.Models
 	internal partial class ResidenciaMetaData 
 	{
         public int id { get; set; }
+        [Display(Name = "Identificación de usuario")]
         public int idUsuario { get; set; }
+        [Display(Name = "Número de individuos")]
         public int individualsNumber { get; set; }
+        [Display(Name = "Año")]
         public string annio { get; set; }
+        [Display(Name = "Estado")]
         public int estado { get; set; }
+        [Display(Name = "Información adicional")]
         public string otherInfoDetails { get; set; }
+        [Display(Name = "Número de casa")]
         public Nullable<int> numeroCasa { get; set; }
 
-
+        public String estadoConverted()
+        {
+            if (this.estado==1)
+            {
+                return "Activo";
+            }
+            else
+            {
+                return "Inactivo";
+            }
+           
+        }
     }
 
     internal partial class UsuarioMetaData
