6c57893898bf2343bd131b53897fbe1383d1f5cd
paopaniar <panipao0@gmail.com>
2023-02-18 19:00:02 -0600

    ServicePlanCobro

--- ApplicationCore/Services/IServicePlanCobro.cs
+++ ApplicationCore/Services/IServicePlanCobro.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Infraestructure.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace ApplicationCore.Services
 {
-    interface IServicePlanCobro
+    public interface IServicePlanCobro
     {
+        IEnumerable<plan_cobro> GetPlanCobro();
     }
 }
---
+++ TerraNostra/Controllers/PlanCobroController.cs
@@ -0,0 +1,36 @@
+﻿using ApplicationCore.Services;
+using Infraestructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TerraNostra.Controllers
+{
+    public class PlanCobroController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            IEnumerable<plan_cobro> lista = null;
+            try
+            {
+                IServicePlanCobro _ServicePlanCobro = new ServicePlanCobro();
+                lista = _ServicePlanCobro.GetPlanCobro();
+                ViewBag.title = "Lista Plan Cobro";
+             
+                return View(lista);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, MethodBase.GetCurrentMethod());
+                TempData["Message"] = "Error al procesar los datos! " + ex.Message;
+
+                // Redireccion a la captura del Error
+                return RedirectToAction("Default", "Error");
+            }
+        }
+    }
+}
