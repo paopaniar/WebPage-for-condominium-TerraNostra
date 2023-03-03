b47707a82df47bc7bac32c720b2abd935af09cf6
MARIANELA ROMERO MENDEZ <mromerom@est.utn.ac.cr>
2023-03-03 14:56:35 -0600

    plan

--- Infraestructure/Models/plan_residencia.cs
+++ Infraestructure/Models/plan_residencia.cs
@@ -19,7 +19,8 @@ namespace Infraestructure.Models
         public int residenciaId { get; set; }
         public string detalle { get; set; }
         public int estado { get; set; }
-    
+
+        public virtual usuario usuario { get; set; }
         public virtual plan_cobro plan_cobro { get; set; }
         public virtual residencia residencia { get; set; }
     }
