36eee4f387443ff267e9b7757ac8be854f7405c9
paopaniar <panipao0@gmail.com>
2023-03-05 09:48:09 -0600

    informacion save n edit

--- TerraNostra/Controllers/InformacionController.cs
+++ TerraNostra/Controllers/InformacionController.cs
@@ -34,6 +34,97 @@ namespace TerraNostra.Controllers
                 return RedirectToAction("Default", "Error");
             }
         }
-    
+
+        private SelectList listUsuarios(int idUsuario = 0)
+        {
+            IServiceUsuario _ServiceUsuario = new ServiceUsuario();
+            IEnumerable<usuario> lista = _ServiceUsuario.GetUsuario();
+            return new SelectList(lista, "identificacion", "nombre", idUsuario);
+        }
+        public ActionResult Edit(int? id)
+        {
+            ServiceInformacion _ServiceInformacion = new ServiceInformacion();
+            informacion informacion = null;
+
+            try
+            {
+                // Si va null
+                if (id == null)
+                {
+                    //ATENCION! HACER EL INDEX byPao
+                    return RedirectToAction("Index");
+                }
+
+                informacion = _ServiceInformacion.GetPlanInformacionById(Convert.ToInt32(id));
+                if (informacion == null)
+                {
+                    TempData["Message"] = "No existe la información solicitada";
+                    TempData["Redirect"] = "Informacion";
+                    TempData["Redirect-Action"] = "Index";
+                    // Redireccion a la captura del Error
+                    return RedirectToAction("Default", "Error");
+                }
+                //Listados
+                ViewBag.idUsuario = listUsuarios(informacion.id);
+
+                return View(informacion);
+            }
+            catch (Exception ex)
+            {
+                // Salvar el error en un archivo 
+                Log.Error(ex, MethodBase.GetCurrentMethod());
+                TempData["Message"] = "Error al procesar los datos! " + ex.Message;
+                TempData["Redirect"] = "Informacion";
+                TempData["Redirect-Action"] = "Index";
+                // Redireccion a la captura del Error
+                return RedirectToAction("Default", "Error");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Save(informacion informacion, string[] selectedUsuarios)
+        {
+
+            //Servicio Libro
+            IServiceInformacion _ServiceInformacion = new ServiceInformacion();
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+                    informacion oInformacionI = _ServiceInformacion.Save(informacion, selectedUsuarios);
+                }
+                else
+                {
+                    // Valida Errores si Javascript está deshabilitado
+                    Utils.Util.ValidateErrors(this);
+                    //  ViewBag.idUsuario = listUsuarios(incidente.id);
+                    ViewBag.id = listUsuarios(informacion.usuario);
+                    //Cargar la vista crear o actualizar
+                    //Lógica para cargar vista correspondiente
+                    if (informacion.id > 0)
+                    {
+                        return (ActionResult)View("Edit", informacion);
+                    }
+                    else
+                    {
+                        return View("Create", informacion);
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                // Salvar el error en un archivo 
+                Log.Error(ex, MethodBase.GetCurrentMethod());
+                TempData["Message"] = "Error al procesar los datos! " + ex.Message;
+                TempData["Redirect"] = "Informacion";
+                TempData["Redirect-Action"] = "Index";
+                // Redireccion a la captura del Error
+                return RedirectToAction("Default", "Error");
+            }
+        }
+
     }
 }
