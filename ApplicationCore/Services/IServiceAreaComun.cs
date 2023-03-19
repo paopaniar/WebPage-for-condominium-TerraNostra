b3c6e1f25707e49d93a25b8a566119c3a76bff27
paopaniar <panipao0@gmail.com>
2023-03-19 09:59:01 -0600

    Service AreaComun

---
+++ ApplicationCore/Services/IServiceAreaComun.cs
@@ -0,0 +1,16 @@
+﻿using Infraestructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services
+{
+    public interface IServiceAreaComun
+    {
+        IEnumerable<areaComun> GetAreaComun();
+        areaComun GetAreaComunById(int id);
+        areaComun Save(areaComun areaComun);
+    }
+}
---
+++ ApplicationCore/Services/ServiceAreaComun.cs
@@ -0,0 +1,31 @@
+﻿using Infraestructure.Models;
+using Infraestructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services
+{
+    public class ServiceAreaComun : IServiceAreaComun
+    {
+        public IEnumerable<areaComun> GetAreaComun()
+        {
+            IRepositoryAreaComun repository = new RepositoryAreaComun();
+            return repository.GetAreaComun();
+        }
+
+        public areaComun GetAreaComunById(int id)
+        {
+            IRepositoryAreaComun repository = new RepositoryAreaComun();
+            return repository.GetAreaComunById(id);
+        }
+
+        public areaComun Save(areaComun areaComun)
+        {
+            IRepositoryAreaComun repository = new RepositoryAreaComun();
+            return repository.Save(areaComun);
+        }
+    }
+}
