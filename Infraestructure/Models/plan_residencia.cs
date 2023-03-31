2e69c5859d3218edf0e753f37ea0928c64b806cb
paopaniar <panipao0@gmail.com>
2023-03-31 16:25:51 -0600

    connection bd

--- Infraestructure/Models/plan_residencia.cs
+++ Infraestructure/Models/plan_residencia.cs
@@ -19,7 +19,7 @@ namespace Infraestructure.Models
         public int residenciaId { get; set; }
         public string detalle { get; set; }
         public int estado { get; set; }
-        public Nullable<System.DateTime> fecha { get; set; }
+        public System.DateTime fecha { get; set; }
     
         public virtual plan_cobro plan_cobro { get; set; }
         public virtual residencia residencia { get; set; }
